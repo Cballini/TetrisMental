700ffd932d3467432a93d9a06e82ab7199adac17
BsahtekLaFauge <valentinfaugier@gmail.com>
2017-07-04 10:18:38 +0200

    Création d'une liste de devices détectés via bluetooth

--- app/src/main/java/com/project/matam/tetris/gamecore/StartActivity.java
+++ app/src/main/java/com/project/matam/tetris/gamecore/StartActivity.java
@@ -1,24 +1,65 @@
 package com.project.matam.tetris.gamecore;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.project.matam.tetris.R;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 /**
  * Created by cecib on 03/07/2017.
  */
 
 public class StartActivity extends AppCompatActivity {
+
+    private final static String BLUETOOTH_LOG_TAG = "Bluetooth handler";
+    private final static int REQUEST_ENABLE_BT = 1;
+    private final static int REQUEST_ACCESS_COARSE_LOCATION = 2;
+
+    private Context context;
+    private ArrayList<String> devicesList;
+    private BluetoothAdapter bluetoothAdapter;
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            String deviceStr; // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+                deviceStr = device.getName() + "\n" + device.getAddress();
+                if (!devicesList.contains(deviceStr)) {
+                    devicesList.add(deviceStr);
+                }
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        context = this;
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        final Button playButton = (Button) findViewById(R.id.bluetooth);
+        devicesList = new ArrayList<String>();
+        final Button playButton = (Button) findViewById(R.id.play);
         playButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -37,5 +78,84 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        final Button bluetoothButton = (Button) findViewById(R.id.bluetooth);
+        bluetoothButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (bluetoothAdapter == null) {
+                    Log.w(BLUETOOTH_LOG_TAG, "Bluetooth is not supported by that device");
+                    return;
+                }
+                if (!bluetoothAdapter.isEnabled()) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                } else {
+                    findDevices();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == REQUEST_ENABLE_BT) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                findDevices();
+            }
+        }
+    }
+
+    private void findDevices() {
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        String test;
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                test = device.getName() + "\n" + device.getAddress();
+                devicesList.add(test);
+            }
+        }
+        setDevicesDiscoverer();
+    }
+
+    private void setDevicesDiscoverer() {
+        // Create a BroadcastReceiver for ACTION_FOUND
+        requestBTPermission();
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        bluetoothAdapter.startDiscovery();
+        registerReceiver(receiver, filter);
+    }
+
+    private void requestBTPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {  // Only ask for these permissions on runtime when running Android 6.0 or higher
+            switch (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                case PackageManager.PERMISSION_DENIED:
+                    if (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(StartActivity.this,
+                                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                REQUEST_ACCESS_COARSE_LOCATION);
+                    }
+                    break;
+                case PackageManager.PERMISSION_GRANTED:
+                    break;
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        bluetoothAdapter.cancelDiscovery();
+        try {
+            unregisterReceiver(receiver);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
     }
 }
